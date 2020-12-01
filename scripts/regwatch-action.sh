a452e4064a69d5b5ed2048bd2ae0cb6b4558c0e9
Leonid Isaev <leonid.isaev@jila.colorado.edu>
2020-12-01 21:10:10 +0000

    scripts/regwatch-action.sh: * Record PID in the logger(1) cmdline; * Use $HOSTNAME instead of a call to hostname(1).

--- scripts/regwatch-action.sh
+++ scripts/regwatch-action.sh
@@ -6,6 +6,6 @@ declare t
 t="$(/usr/bin/cat -)"
 
 echo -E "Please check your server..." | /usr/bin/mail -s \
-	"SIP registration failed at $t on host \"$(hostname)\"" \
+	"SIP registration failed at $t on host \"${HOSTNAME}\"" \
 	"${sendto[@]}" 2>&1 | \
-	/usr/bin/logger -t "regwatch-action" --
+	/usr/bin/logger -t "${0##*/}($$)" --
