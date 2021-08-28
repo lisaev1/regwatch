536b1769a09b97ff5c4c66817df8c226e260ce25
Leonid Isaev <leonid.isaev@jila.colorado.edu>
2021-08-28 22:49:46 +0000

    * scripts/regwatch-action.sh:   - Make sure that the connection token is verified;   - Add date to the notification email; * systemd/regwatch.service:     Switch to the python3 implementation.

--- scripts/regwatch-action.sh
+++ scripts/regwatch-action.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+declare -r TOKEN="ndT1LkaJP6SgIfj2FdkUo1E7tSXgoU"
+declare -ra LOGGER=("/usr/bin/logger" "-t" "${0##*/}($$)" "--")
+
 declare -a sendto=("leonid.isaev@ifax.com")
 declare t
 
 t="$(/usr/bin/cat -)"
 
+if [[ "$t" != "$TOKEN" ]]; then
+	"${LOGGER[@]}" \
+		"Unauthorized connection attempt with token \"${t}\""
+	exit 1
+fi
+
 echo -E "Please check your server..." | /usr/bin/mail -s \
-	"SIP registration failed at $t on host \"${HOSTNAME}\"" \
-	"${sendto[@]}" 2>&1 | \
-	/usr/bin/logger -t "${0##*/}($$)" --
+    "SIP registration failed on \"$(/usr/bin/date)\" on host \"${HOSTNAME}\"" \
+	"${sendto[@]}" 2>&1 | "${LOGGER[@]}"
